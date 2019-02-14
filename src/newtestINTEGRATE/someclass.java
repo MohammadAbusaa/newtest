3515961b0c72a2e957363c7552b10ee22dfc7f5c
MohammadAbusaa <47559635+MohammadAbusaa@users.noreply.github.com>
2019-02-14 16:11:28 +0300

    random code

---
+++ src/newtestINTEGRATE/someclass.java
@@ -0,0 +1,15 @@
+package newtestINTEGRATE;
+
+import java.util.Scanner;
+
+public class someclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc=new Scanner(System.in);
+		int x =sc.nextInt();
+		System.out.println(Integer.toString(x));
+	}
+
+}
