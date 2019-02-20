8d0096290d0977d65bf5dde21978d6b558737c81
MohammadAbusaa <47559635+MohammadAbusaa@users.noreply.github.com>
2019-02-20 22:29:31 +0300

    TRY

---
+++ src/newtestINTEGRATE/newtest.java
@@ -0,0 +1,17 @@
+package newtestINTEGRATE;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class newtest {
+
+	someclass x;
+	@Test
+	void test() {
+		
+		x=new someclass();
+		assertTrue(condition);
+	}
+
+}
